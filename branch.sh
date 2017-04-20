ab1863919c60ba40a3393355468049270ccf598d
Chandrasekaran <chandrk9@emea.roche.com>
2017-04-20 08:57:13 +0200

    update

--- branch.sh
+++ branch.sh
@@ -3,9 +3,9 @@
 while read line
 do
 branch_name=$line
-git push origin master:refs/heads/$line
-echo "git push origin master:refs/heads/$line"
-git fetch origin
+#git push origin master:refs/heads/$line
+#echo "git push origin master:refs/heads/$line"
+#git fetch origin
 git checkout --track -b $line origin/$line
 git pull
 git checkout master
