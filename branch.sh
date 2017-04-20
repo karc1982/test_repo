990f973a5daa586cc6a8432e4ebfc6f9b7635afa
Chandrasekaran <chandrk9@emea.roche.com>
2017-04-20 08:54:11 +0200

    updated script

--- branch.sh
+++ branch.sh
@@ -3,10 +3,10 @@
 while read line
 do
 branch_name=$line
-git push origin master:refs/heads/$branch_name
-echo "git push origin master:refs/heads/$branch_name"
+git push origin master:refs/heads/$line
+echo "git push origin master:refs/heads/$line"
 git fetch origin
-git checkout --track -b $branch_name origin/$branch_name
+git checkout --track -b $line origin/$line
 git pull
 git checkout master
-done < C:\git\branch.txt
+done < branch.txt
